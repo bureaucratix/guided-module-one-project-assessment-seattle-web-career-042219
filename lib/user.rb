36a1580668c1407af6259e76311a1bba877ee7a2
github username <github email address>
2019-05-09 16:26:45 -0700

    Added compare function for QoL Metrics

--- lib/user.rb
+++ lib/user.rb
@@ -12,11 +12,19 @@ class User < ActiveRecord::Base
 
  def get_city_array
    City.left_outer_joins(:user_cities).where('user_cities.user_id = ?', self.id)
+
  end
 
   def find_user_city_by_city_id(city_array, city_index)
     UserCity.find_by(user_id: self.id, city_id: city_array[city_index.to_i-1].id)
   end
 
+  def sort_cities_by_metric(metric)
+    table = TTY::Table.new header: ['City', 'Urban Area', Qol_string_hash[metric]]
+    self.get_city_array.each do |city|
+      table << [city.name, UrbanArea.find(city.urban_area_id).name, UrbanArea.find(city.urban_area_id)[metric].round(2)]
+    end
+    puts table.render :ascii, alignment: [:left]
+  end
 
 end
